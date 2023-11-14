f4e327ce33486b6273ad81015eac8e9907c254f5
llukezin <lucassmendonca77@gmail.com>
2023-11-14 17:33:44 -0300

    exportando arquivo

---
+++ microblog_lucas.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 14-Nov-2023 às 21:27
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `microblog_lucas`
+--
+CREATE DATABASE IF NOT EXISTS `microblog_lucas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `microblog_lucas`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `id` int(11) NOT NULL,
+  `data` datetime NOT NULL DEFAULT current_timestamp(),
+  `titulo` varchar(150) NOT NULL,
+  `texto` text NOT NULL,
+  `resumo` text NOT NULL,
+  `imagem` varchar(45) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `noticias`
+--
+
+INSERT INTO `noticias` (`id`, `data`, `titulo`, `texto`, `resumo`, `imagem`, `usuario_id`) VALUES
+(1, '2023-11-14 16:25:06', 'Descoberto oxigênio em Vênus', 'Nesta manhã, uma decoberta incrivel foi feitas por astronomos a fina atmosfera marciana é composta por 96% de dióxido de carbono, tornando o ar irrespirável para os humanos. O Moxie funciona dividindo moléculas de dióxido de carbono, que incluem um átomo de carbono e dois átomos de oxigênio.', 'Recentemente o telescopio no Havai encontrou traços de oxigênio no planeta...', 'venus,jpg', 1),
+(2, '2023-11-14 16:36:34', 'Nova versão do VSCode', 'A Microsoft trouxe recursos de Inteligência Artificial...', 'Recentemente o VSCode foi atualizado...', 'vscode,png', 4),
+(3, '2023-11-14 16:36:34', 'Onda de calor no Brasil', 'Efeitos do aquecimentos global estão prejudicando a vida...', 'Temperaturas muito acima da média', 'sol.svg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo` enum('admin','editor') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`) VALUES
+(1, 'Luke Mendonça', 'llukezinnegocios@gmail.com', '40028922', 'admin'),
+(3, 'Beltrano Soares', 'beltrano@msn.com', '000penha', 'admin'),
+(4, 'Chapolin Colorado', 'chapolin@vingadores.com.br', 'marreta', 'admin');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_noticias_usuarios` (`usuario_id`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `noticias`
+--
+ALTER TABLE `noticias`
+  ADD CONSTRAINT `fk_noticias_usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
