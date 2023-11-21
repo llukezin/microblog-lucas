835ed1584561987af6c256a1d29c7ec5692f6e88
llukezin <lucassmendonca77@gmail.com>
2023-11-21 17:29:39 -0300

    terminamos por hoje, ate amanha

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -6,10 +6,34 @@ require_once "../inc/funcoes-usuarios.php";
 $id = $_GET['id'];
 
 // Chamando a função e guardando o retorno dela
-$dados = lerUmUsuario($conexao, $id);
+$usuario = lerUmUsuario($conexao, $id);
+// Verificando se o formulario foi acionado
+if(isset($_POST ['atualizar'])){
+	// Capturando os dados
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$tipo = $_POST['tipo'];
+
+	/* Lógica para a senha
+	Se o campo senha estiver vazio OU se a senha digitada for igual á senha que já existe no banco de dados, então significa que o usuário NÃO ALTEROU A SENHA. Portanto, devemos MANTER a senha existente.*/
+	if( empty($_POST['senha']) ||
+	    password_verify($_POST['senha'], $usuario['senha'] ) ) {
+		
+		$senha = $usuario['senha']; // mantemos a mesma
+
+	} else {
+	/* Caso contrário, pegarmos a senha nova digitada e a CODIFICAMOS antes de manter para o banco. */
+	$senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+	}
+
+	//chamamos a função e passamos os dados
+	atualizarUsuario($conexao, $id, $nome, $email, $senha, $tipo);
+
+	// Redirecionamos para a página de usuarios
+	header("location:usuarios.php");
+}
 ?>
 
-<pre> <?=var_dump($dados)?> </pre>
 
 
 <div class="row">
@@ -23,12 +47,12 @@ $dados = lerUmUsuario($conexao, $id);
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input value="<?=$usuario['nome']?>" class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input value="<?=$usuario['email']?>" class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
@@ -39,9 +63,17 @@ $dados = lerUmUsuario($conexao, $id);
 			<div class="mb-3">
 				<label class="form-label" for="tipo">Tipo:</label>
 				<select class="form-select" name="tipo" id="tipo" required>
+
 					<option value=""></option>
-					<option value="editor">Editor</option>
-					<option value="admin">Administrador</option>
+
+					<option
+					<?php if($usuario["tipo"] === "editor") echo "selected"; ?>
+					value="editor">Editor</option>
+
+					<option
+					<?php if($usuario["tipo"] === "admin") echo "selected"; ?>
+					value="admin">Administrador</option>
+				
 				</select>
 			</div>
 			
--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -35,3 +35,15 @@ function lerUmUsuario($conexao, $id){
     // retornando p resultado trasnformado em Um array com os dados
     return mysqli_fetch_assoc($resultado);
 }
+
+function atualizarUsuario( $conexao, $id, $nome, $email, $senha, $tipo ){
+    $sql = "UPDATE usuarios SET 
+                nome = '$nome',
+                email = '$email', 
+                senha = '$senha',
+                tipo = '$tipo'
+            WHERE id = $id"; // NÃO SE ESQUEÇA DESSA BAGAÇA POR FAVOR!! PERIGO!
+
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+}
