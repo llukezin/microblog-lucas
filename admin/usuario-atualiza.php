e7e3f83a2cfa0b591c0ca2e5d01b782dd46718d5
llukezin <lucassmendonca77@gmail.com>
2023-11-21 16:31:38 -0300

    uns negocios ai

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -1,6 +1,15 @@
 <?php 
 require_once "../inc/cabecalho-admin.php";
+require_once "../inc/funcoes-usuarios.php";
+
+// Pegando o valor do parâmetro id vindo da URL
+$id = $_GET['id'];
+
+// Chamando a função e guardando o retorno dela
+$dados = lerUmUsuario($conexao, $id);
 ?>
+
+<pre> <?=var_dump($dados)?> </pre>
 
 
 <div class="row">
--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -24,3 +24,14 @@ function lerUsuarios($conexao){
     // Retornamos  resultado da query transformando em array associativo
     return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
 }
+
+function lerUmUsuario($conexao, $id){
+    // Montamos o sql contendo o id do usuário que queremos carregar
+    $sql = "SELECT * FROM usuarios WHERE id = $id";
+
+    // executamos e guardamos o resultado da consulta
+    $resultado = mysqli_query($conexao, $sql) or die (mysqli_error($conexao));
+
+    // retornando p resultado trasnformado em Um array com os dados
+    return mysqli_fetch_assoc($resultado);
+}
