494685370918f2893f7170c9d74ee33fa53878d7
llukezin <lucassmendonca77@gmail.com>
2023-11-30 17:25:20 -0300

    finalizaçao por hj

--- noticia.php
+++ noticia.php
@@ -13,7 +13,7 @@ $dadosDaNoticia = lerDetalhes($conexao, $idNoticia);
     <article class="col-12">
         <h2><?=$dadosDaNoticia['titulo']?></h2>
         <p class="font-weight-light">
-            <time><?=$dadosDaNoticia['data']?></time> - <span><?=$dadosDaNoticia['autor']?></span>
+            <time><?=formataData($dadosDaNoticia['data'])?></time> - <span><?=$dadosDaNoticia['autor']?></span>
         </p>
         <img src="imagens/<?=$dadosDaNoticia['imagem']?>"class="float-start pe-2 img-fluid">
         <p class="ajusta-texto"><?=$dadosDaNoticia['texto']?></p>
